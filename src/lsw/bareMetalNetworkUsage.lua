d0b2c508c98aa0e58ec789094b17043eb74405c3
crito <crito@fnordpipe.org>
2017-09-22 15:55:14 +0200

    api: add network usage operations and refactore restclient
    restclient is still compatible with the other callers.
    This commit drops duplicate code lines and add support for
    accepted response image/png on get requests.

---
+++ src/lsw/bareMetalNetworkUsage.lua
@@ -0,0 +1,41 @@
+local lswRest = require('lsw.rest')
+
+local module = {}
+
+function module.init(self, apiKey, bareMetalId)
+  module.apiKey = apiKey
+  local network = {}
+
+  network.retrieveUsage = function(context, from, to, png)
+    local u = '/v1/bareMetals/' .. bareMetalId .. '/networkUsage' .. context
+    local r = { dateFrom = from, dateTo = to }
+    local s, nu = nil, nil
+
+    if png then
+      s, nu = lswRest:getPng(u, apiKey, r)
+    else
+      s, nu = lswRest:get(u, apiKey, r)
+    end
+
+    if s == 200 then
+      return nu
+    end
+    return nil
+  end
+
+  network.retrieveNetworkUsage = function(from, to, png)
+    return network.retrieveUsage('', from, to, png)
+  end
+
+  network.retrieveBandwidthUsage = function(from, to, png)
+    return (network.retrieveUsage('/bandwidth', from, to, png) or {})['bandwidth']
+  end
+
+  network.retrieveDataTrafficUsage = function(from, to, png)
+    return (network.retrieveUsage('/datatraffic', from, to, png) or {})['datatraffic']
+  end
+
+  return network
+end
+
+return module
--- src/lsw/rest.lua
+++ src/lsw/rest.lua
@@ -16,88 +16,56 @@ local _toTable = function(s)
   return json.decode(s)
 end
 
-function rest.get(self, url, apiKey)
+function rest.call(self, url, method, apiKey, header, request)
+  local reqBody = nil
+  local headers = header or {}
+  headers['x-lsw-auth'] = apiKey
+
+  if request then
+    reqBody = _toJson(request)
+    headers['content-type'] = 'application/json'
+    headers['content-length'] = #reqBody
+    reqBody = ltn12.source.string(reqBody)
+  end
   local respBody = {}
   local resp, respStatus, respHeader = https.request{
-    method = 'GET',
-    headers = {
-      ['x-lsw-auth'] = apiKey
-    },
+    method = method,
+    headers = headers,
+    source = reqBody,
     sink = ltn12.sink.table(respBody),
     url = rest.api .. url
   }
   if respHeader['content-type']:match('application/json') then
     return respStatus, _toTable(respBody[1])
+  elseif respHeader['content-type']:match('image/png') then
+    return respStatus, respBody[1]
   else
-    print(respBody[1])
+    return respStatus
   end
   return nil
+end
+
+function rest.delete(self, url, apiKey)
+  return rest:call(url, 'DELETE', apiKey, nil, nil)
+end
+
+function rest.get(self, url, apiKey, request)
+  return rest:call(url, 'GET', apiKey, request, nil)
+end
+
+function rest.getPng(self, url, apiKey, request)
+  local h = {
+    ['Accept'] = 'image/png'
+  }
+  return rest:call(url, 'GET', apiKey, request, nil)
 end
 
 function rest.post(self, url, apiKey, request)
-  local reqBody = nil
-  local headers = {
-    ['x-lsw-auth'] = apiKey
-  }
-  if request then
-    reqBody = _toJson(request)
-    headers['content-type'] = 'application/json'
-    headers['content-length'] = #reqBody
-    reqBody = ltn12.source.string(reqBody)
-  end
-  local respBody = {}
-  local resp, respStatus, respHeader = https.request{
-    method = 'POST',
-    headers = headers,
-    source = reqBody,
-    sink = ltn12.sink.table(respBody),
-    url = rest.api .. url
-  }
-  if respHeader['content-type']:match('application/json') then
-    return respStatus, _toTable(respBody[1])
-  else
-    print(respBody[1])
-  end
-  return nil
+  return rest:call(url, 'POST', apiKey, request, nil)
 end
 
 function rest.put(self, url, apiKey, request)
-  local reqBody = nil
-  local headers = {
-    ['x-lsw-auth'] = apiKey
-  }
-  if request then
-    reqBody = _toJson(request)
-    headers['content-type'] = 'application/json'
-    headers['content-length'] = #reqBody
-    reqBody = ltn12.source.string(reqBody)
-  end
-  local respBody = {}
-  local resp, respStatus, respHeader = https.request{
-    method = 'PUT',
-    headers = headers,
-    source = reqBody,
-    sink = ltn12.sink.table(respBody),
-    url = rest.api .. url
-  }
-  if respHeader['content-type']:match('application/json') then
-    return respStatus, _toTable(respBody[1])
-  else
-    print(respBody[1])
-  end
-  return nil
-end
-
-
-function rest.delete(self, url, apiKey)
-  local resp, respStatus, respHeader = https.request{
-    method = 'DELETE',
-    headers = {
-      ['x-lsw-auth'] = apiKey
-    },
-    url = rest.api .. url
-  }
-  return respStatus
+  return rest:call(url, 'PUT', apiKey, request, nil)
 end
 
 return rest
